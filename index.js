08a1cd04d0766137dd8d325198200598a2330bd1
Aryan Patel <aryanpatel31@yahoo.com>
2023-12-15 07:47:40 -0600

    App created, basic chat functionality working

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+import express from 'express'
 import { config } from 'dotenv'
+import { OpenAI } from 'openai'
+import cors from 'cors';
+
+
 config()
 
-import { OpenAI } from 'openai'
-import readline from 'readline'
+const app = express()
+const port = process.env.PORT || 3000;
 
 const openai = new OpenAI( { apiKey: process.env.API_KEY } );
 
-const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+app.use(express.json());
+app.use(cors());
+
+app.post('/chat', async (req, res) => {
+  const input = req.body.input;
+
+  if (!input) {
+    return res.status(400).json({ error: 'Input is required' });
+  }
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: input }],
+    });
+
+    const output = response.choices[0].message.content;
+
+    res.json({ output });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 })
 
-userInterface.prompt()
-userInterface.on("line", async input => {
-  const response = await openai.chat.completions.create({ 
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-})
-console.log(response.choices[0].message.content)
-userInterface.prompt()
-})
+app.listen(port, () => {
+  console.log('Server is running on port ${port}');
+});
---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const chatForm = document.getElementById('chatForm');
+  const userInput = document.getElementById('userInput');
+  const responseDiv = document.getElementById('response');
+
+  chatForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const userMessage = userInput.value;
+
+    fetch('http://localhost:3000/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input: userMessage }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        responseDiv.innerText = data.output;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        responseDiv.innerText = 'An error occurred. Please try again.';
+      });
+  });
+});
